0ec12b2a49d70eb5f73b8c98f6f649b44b4d8a92
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-10 19:48:38 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      'https://eleventh-a-roomready.web.app',
+      'https://eleventh-a-roomready.firebaseapp.com'
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser())
+
+const user = process.env.DB_USER
+const password = process.env.DB_PASS
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.ahaugjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('RoomReady Server Running!')
