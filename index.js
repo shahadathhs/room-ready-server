296175f2fd90809f6b6e8856467e7ce65c3c6b2c
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-11 21:30:51 +0600

    booking delete api added

--- index.js
+++ index.js
@@ -155,6 +155,14 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      console.log("hitted")
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
