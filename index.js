f0bdee84f0183c671273c02114a173b0cc8abe63
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-12 17:21:54 +0600

    stord reviews send to client side

--- index.js
+++ index.js
@@ -225,6 +225,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/reviews", async (req, res) => {
+      const reviews = req.query;
+      const cursor = reviewsCollection.find(reviews);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
