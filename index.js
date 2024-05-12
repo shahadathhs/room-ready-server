deea8687208816904314637aec38d14227d8abaf
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-12 09:49:53 +0600

    booking schedule updated using patch method

--- index.js
+++ index.js
@@ -169,19 +169,19 @@ async function run() {
       res.send(result);
     })
 
-    // app.patch("/bookings/:id", async(req, res) => {
-    //   const id = req.params.id;
-    //   const filter ={ _id : new ObjectId(id)}
-    //   const booking = req.body;
-    //   console.log(booking)
-    //   const updatedDoc = {
-    //     $set:{
-    //       schedule: booking.schedule
-    //     }
-    //   }
-    //   const result =await bookingsCollection.updateOne(filter,updatedDoc)
-    //   res.send(result)
-    // })
+    app.patch("/bookings/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter ={ _id : new ObjectId(id)}
+      const booking = req.body;
+      console.log(booking)
+      const updatedDoc = {
+        $set:{
+          schedule: booking.schedule
+        }
+      }
+      const result =await bookingsCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
